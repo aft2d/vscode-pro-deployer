3817ed72d27e1fde6546cb5fee3b949f6c678306
Roman Steinhart <roman@aternos.org>
2026-02-03 13:03:59 +0100

    Support SSH agent authentication

--- src/targets/SFTP.ts
+++ src/targets/SFTP.ts
@@ -8,6 +8,7 @@ import { Configs } from "../configs";
 import EventEmitter = require("events");
 import { Targets } from "./Targets";
 import { Target } from "./Target";
+import * as process from "process";
 
 export class SFTP extends Target implements TargetInterface {
     private client = new ssh2.Client();
@@ -174,6 +175,12 @@ export class SFTP extends Target implements TargetInterface {
         Extension.appendLineToOutputChannel(
             "[INFO][SFTP] Connecting to: " + this.options.host + ":" + this.options.port
         );
+        if (!this.options.useAuthAgent) {
+            this.options.useAuthAgent = false;
+        }
+        Extension.appendLineToOutputChannel(
+            "[INFO][SFTP] Auth agent: " + (this.options.useAuthAgent ? ("Enabled (" + this.options.authAgentPath || process.env.SSH_AUTH_SOCK + ")") : "Disabled")
+        );
         this.client.connect({
             host: this.options.host,
             port: this.options.port,
@@ -182,6 +189,7 @@ export class SFTP extends Target implements TargetInterface {
             privateKey: privateKey,
             passphrase: this.options.passphrase,
             tryKeyboard: true, // Enable keyboard-interactive fallback
+            agent: this.options.useAuthAgent ? (this.options.authAgentPath || process.env.SSH_AUTH_SOCK) : undefined,
         });
     }
     upload(uri: vscode.Uri): Promise<vscode.Uri> {
